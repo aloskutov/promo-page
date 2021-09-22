18dff1d832d2e4c2aa0c3d1790e5eeba0846459e
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-09-22 15:59:36 +0700

    added smoothscroll

---
+++ js/smoothscroll.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const linksHead = document.querySelectorAll('.menu-list__link');
+
+linksHead.forEach(item => {
+    item.addEventListener('click', event => {
+        const linkId = event.target.getAttribute('href').replace('#', '')
+
+        window.scrollTo({
+            top: document.getElementById(linkId).offsetTop,
+            behavior: 'smooth',
+        });
+
+        event.preventDefault();
+    });
+});
