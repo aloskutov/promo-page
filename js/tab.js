96f8b9b17a16eb6bed8702ca0980ab450a06526c
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-09-22 14:58:42 +0700

    added tabs.js

---
+++ js/tab.js
@@ -0,0 +1,27 @@
+const tabContainer = document.querySelector('.design-block');
+const tabButtons = tabContainer.querySelectorAll('.design-list__item');
+const tabHeaders = tabContainer.querySelectorAll('.design__title');
+const tabImages = tabContainer.querySelectorAll('.design-block__img');
+const tabDescription = tabContainer.querySelectorAll('.design__descr');
+const tabItems = [tabHeaders, tabImages, tabDescription];
+
+tabButtons.forEach((element, index) => {
+    element.addEventListener('click', event => {
+        tabButtons.forEach(item => {
+            if (item.classList.contains('design-list__item_active')) {
+                item.classList.remove('design-list__item_active');
+            }
+        });
+
+        event.target.classList.add('design-list__item_active');
+
+        tabItems.forEach(tabItem => {
+            tabItem.forEach(item => {
+                if (!item.classList.contains('hidden')) {
+                    item.classList.add('hidden');
+                }
+            });
+            tabItem[index].classList.remove('hidden');
+        });
+    });
+});
