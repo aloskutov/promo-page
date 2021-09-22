d479f12f2b65c71e0fa4360610d63d8d0b892220
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-09-22 13:21:42 +0700

    added accordion.js

---
+++ js/accordion.js
@@ -0,0 +1,27 @@
+const accordion = document.querySelector('.feature-list');
+const accordionButtons = document.querySelectorAll('.feature__link');
+
+accordionButtons.forEach(item => {
+    item.addEventListener('click', accordionAction);
+});
+
+function accordionAction(event){
+    console.log(event.target);
+    const targetElement = event.target;
+    const targetItem = targetElement.closest('.feature__item');
+    const targetLink = targetItem.querySelector('.feature__link');
+    const targetSub = targetItem.querySelector('.feature-sub');
+    const activeLink = accordion.querySelector('.feature__link_active');
+
+    if (targetLink.classList.contains('feature__link_active')) {
+        targetLink.classList.remove('feature__link_active');
+        targetSub.classList.add('hidden');
+    } else {
+        if (activeLink) {
+            activeLink.classList.remove('feature__link_active');
+            activeLink.closest('.feature__item').querySelector('.feature-sub').classList.add('hidden');
+        }
+        targetLink.classList.add('feature__link_active');
+        targetSub.classList.remove('hidden');
+    }
+};
